f2970fdaf66341faca94cfdd618f6b86a0e063bf
ByboCZ <jan.papuga7@gmail.com>
2024-01-26 16:04:53 +0100

    MainMenu, Tiles
    Mainmenu přidané ale zatím nefunguje
    Můžeme zmáčknout E a "Tiles" zmizí

--- Factor/Assets/Scripts/Controllers/WorldController.cs
+++ Factor/Assets/Scripts/Controllers/WorldController.cs
@@ -7,12 +7,17 @@ public class WorldController : MonoBehaviour
     World world;
     public Sprite floorSprite;
 
+    bool tilesVisible = true;
+
+    // List pro uchovávání všech SpriteRenderers
+    List<SpriteRenderer> tileRenderers = new List<SpriteRenderer>();
+
     // Start is called before the first frame update
     void Start()
     {
         world = new World();
 
-        //Vytvoøím gameObject, který vizuálnì ukáže tiles
+        // Vytvoøím gameObject, který vizuálnì ukáže tiles
         for (int x = 0; x < world.width; x++)
         {
             for (int y = 0; y < world.height; y++)
@@ -22,16 +27,29 @@ public class WorldController : MonoBehaviour
                 GameObject tile_go = new GameObject();
                 tile_go.name = "Tile_" + x + "_" + y;
                 tile_go.transform.position = new Vector3(x, y, 0);
+                tile_go.tag = "Tiles";
 
                 SpriteRenderer tile_sr = tile_go.AddComponent<SpriteRenderer>();
                 tile_sr.sprite = floorSprite;
+
+                // Pøidání SpriteRenderer do listu
+                tileRenderers.Add(tile_sr);
             }
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Sledování stisknutí klávesy E
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            tilesVisible = !tilesVisible;
+
+            // Nastavení viditelnosti pro všechny SpriteRenderers v listu
+            foreach (SpriteRenderer tileRenderer in tileRenderers)
+            {
+                tileRenderer.enabled = tilesVisible;
+            }
+        }
     }
 }
